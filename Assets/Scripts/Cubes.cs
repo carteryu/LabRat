9dbde3f3d4120344d347ec8efa860058576e9c48
allyhandro <awh264@gmail.com>
2015-12-14 08:59:34 -0500

    some scripting but nothing really

---
+++ Assets/Scripts/Cubes.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cubes : MonoBehaviour {
+	public GameObject[] mazeBlocks;
+	public float resetDelay = 2f;
+	// Use this for initialization
+	void Start () {
+		mazeBlocks = GameObject.FindGameObjectsWithTag("mazeBlock");
+		
+		for(int i = 0; i < mazeBlocks.Length; i++){
+			Debug.Log("tile: " + i+ "is growing");
+		}
+	}
+	
+	void Grow(){
+		transform.localScale += new Vector3(0, 9, 0);
+	}
+}
